36e119a2379c385ab3cce878ccc6bc2d05dc42a1
Cong Thanh Nguyen <thanhcong.bk10@gmail.com>
2020-01-20 22:33:01 +0700

    Implement assign operator, literal

--- assignment/initial/src/test/CodeGenSuite.py
+++ assignment/initial/src/test/CodeGenSuite.py
@@ -4,25 +4,36 @@ from AST import *
 
 
 class CheckCodeGenSuite(unittest.TestCase):
-
-    def test_int(self):
+    #Check Literal
+    def test_int_lit(self):
         """Simple program: int main() {} """
         input = """void main() {putInt(100);}"""
         expect = "100"
         self.assertTrue(TestCodeGen.test(input,expect,500))
-    def test_int_ast(self):
-        input = """void main() {
-                    putInt(100);
-                }"""
-        expect = "100"
-        self.assertTrue(TestCodeGen.test(input,expect,500))
 
-    def test_float_ast(self):
+    def test_float_lit(self):
         input = """void main() {
                     putFloat(100.001);
                 }"""
         expect = "100.001"
         self.assertTrue(TestCodeGen.test(input,expect,500))
+
+    def test_boolean_lit(self):
+        input = """void main() {
+                    putBoolLn(true);
+                    putBool(false);
+                }"""
+        expect = "true\nfalse"
+        self.assertTrue(TestCodeGen.test(input,expect,500))
+
+    def test_string_lit(self):
+        input = """void main() {
+                    putString("This is a string");
+                }"""
+        expect = "This is a string"
+        self.assertTrue(TestCodeGen.test(input,expect,500))
+
+
 
     def test_int_vs_int(self):
         input = """void main() {
@@ -117,14 +128,38 @@ class CheckCodeGenSuite(unittest.TestCase):
                     putFloat(sum_float(1001,10));
                 }"""
         expect = "1011.0"
-        self.assertTrue(TestCodeGen.test(input,expect,501))
-    '''
-    def test_id(self):
+        self.assertTrue(TestCodeGen.test(input,expect,500))
+
+    def test_func_using_before_define(self):
+        input = """
+                void main() {
+                    putFloat(sum_float(1001,10));
+                }
+                float sum_float(int a, int b){
+                    return a + b;
+                }
+                """
+        expect = "1011.0"
+        self.assertTrue(TestCodeGen.test(input,expect,500))
+
+    def test_assign_op_simple(self):
         input = """void main() {
                     int a;
                     a = 100;
-                    putInt(a);
+                    putIntLn(a);
+                    float b;
+                    b = 1.1;
+                    putFloat(b);
+
                 }"""
-        expect = "100"
-        self.assertTrue(TestCodeGen.test(input,expect,507))
-    '''
+        expect = "100\n1.1"
+        self.assertTrue(TestCodeGen.test(input,expect,500))
+
+    def test_assign_op_int_vs_float(self):
+        input = """void main() {
+                    float a;
+                    a = 100;
+                    putFloat(a);
+                }"""
+        expect = "100.0"
+        self.assertTrue(TestCodeGen.test(input,expect,500))
